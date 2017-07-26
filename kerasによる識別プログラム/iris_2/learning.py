8d6ab50e2c60c9a206dd6059045778142d48105c
Katsuhiro Morishita <morimori.ynct@gmail.com>
2017-07-27 04:08:56 +0900

    ラベル自動変換と整数ラベルへの対応
    出力が出力層のユニットの出力だったものをラベルに変更した。また、文字列のラベルにしか対応していなかったが、0から始まる整数のラベルにも対応した。
    また、データ読み込み時のスライスの書き方を変更している。

--- kerasによる識別プログラム/iris_2/learning.py
+++ kerasによる識別プログラム/iris_2/learning.py
@@ -1,7 +1,7 @@
 # purpose: kerasによるiris識別プログラム　学習編
-# 教師データの正解ラベルは文字列でクラスが指定されている事を想定している。
+# memo: 教師データの正解ラベルは文字列でクラスが指定されている事を想定している。
 # author: Katsuhiro MORISHITA　森下功啓
-# memo: 読み込むデータは、1行目に列名があり、最終列に層名（クラス名）が入っていること。
+# memo: 読み込むデータは、1行目に列名があり、最終列に層名（文字列でクラス名、または整数で0-nの連番）が入っていること。
 # created: 2017-07-08
 import numpy as np
 import pandas
@@ -10,44 +10,56 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras.utils import np_utils
 from sklearn import preprocessing # 次元毎の正規化に使う
-from sklearn.preprocessing import OneHotEncoder # 判別問題における数値の正解ラベルをベクトル化するライブラリ
 from sklearn.feature_extraction import DictVectorizer # 判別問題における文字列による正解ラベルをベクトル化する
 
 
 
 # データの読み込み
-df = pandas.read_csv("iris_learning.csv")
+df = pandas.read_csv("iris_learning_str_label.csv")
 df = df.reindex(np.random.permutation(df.index)).reset_index(drop=True) # ランダムに並べ替える（効果高い）
-x = (df.iloc[:, 0:4]).values # transform to ndarray
+s = len(df.columns)
+x = (df.iloc[:, :-1]).values # transform to ndarray
 x = preprocessing.scale(x) # 次元毎に正規化する
-y = (df.iloc[:, 4:5]).values
+y = (df.iloc[:, -1:]).values
 print("x", x)
 print("y", y)
 
+
 # 正解ラベルを01のリストを作成
-vec = DictVectorizer()
-y = vec.fit_transform([{"class":mem[0]} for mem in y]).toarray() # 判別問題における文字列による正解ラベルをベクトル化する
+labels = np.ravel(y) # 出力をラベルに変換するための布石
+if y.dtype == "object":  # ラベルが文字列かチェック
+	vec = DictVectorizer()
+	y = vec.fit_transform([{"class":mem[0]} for mem in y]).toarray() # 判別問題における文字列による正解ラベルをベクトル化する
+else:
+	y = np_utils.to_categorical(y)
 print("y", y)
+label_dict = {list(y[i]).index(y[i].max()):labels[i] for i in range(len(labels))} # 出力をラベルに変換する辞書
+with open('label_dict.pickle', 'wb') as f:
+	pickle.dump(label_dict, f)
 
 # 学習器の準備
 model = Sequential()
-model.add(Dense(2, input_shape=(4, ))) # 入力層は全結合層で入力が4次元のベクトルで、出力先のユニット数が16。活性化関数はなし（または次で定義）。
-model.add(Activation('relu')) # 全結合層で、活性化関数がReLU。この書き方だと、上のdenseがReLUであると指定している
-model.add(Dense(3))
+model.add(Dense(2, input_shape=(s-1, ))) # 入力層は全結合層で入力がs-1次元のベクトルで、出力先のユニット数が2。
+model.add(Activation('relu')) # 中間層の活性化関数がReLU
+model.add(Dense(len(y[0]))) # 出力層のユニット数
+model.add(Activation('relu')) # 中間層の活性化関数がReLU
 model.add(Activation('softmax')) # 出力が合計すると1になる
 model.compile(optimizer='adam',
       loss='categorical_crossentropy', # binary_crossentropy
       metrics=['accuracy'])
 
 # 学習
-model.fit(x, y, epochs=500, batch_size=50, verbose=1) # nb_epochは古い引数の書き方なので、epochsを使う@2017-07
+batch_size = 5
+model.fit(x, y, epochs=500, batch_size=batch_size, verbose=1) # nb_epochは古い引数の書き方なので、epochsを使う@2017-07
 
 # 学習のチェック
-result = model.predict_classes(x, batch_size=5, verbose=0) # クラス推定
+result = model.predict_classes(x, batch_size=batch_size, verbose=0) # クラス推定
 print("result1: ", result)
-result = model.predict(x, batch_size=5, verbose=0) # 各ユニットの出力を見る
+for mem in result:
+	print("label convert test,", mem, label_dict[mem])
+result = model.predict(x, batch_size=batch_size, verbose=0) # 各ユニットの出力を見る
 print("result2: ", result)
-result = model.predict_proba(x, batch_size=5, verbose=0) # 確率を出す（softmaxを使っているので、predictと同じ出力になる）
+result = model.predict_proba(x, batch_size=batch_size, verbose=0) # 確率を出す（softmaxを使っているので、predictと同じ出力になる）
 print("result3: ", result)
 
 
--- kerasによる識別プログラム/iris_2/prediction.py
+++ kerasによる識別プログラム/iris_2/prediction.py
@@ -1,9 +1,7 @@
-#----------------------------------------
 # purpose: kerasによる弁別器テストスクリプト　予測編
 # author: Katsuhiro MORISHITA　森下功啓
-# memo: 読み込むデータは、1行目に列名があり、最終列に層名（クラス名）が入っていること。
+# memo: 読み込むデータは、1行目に列名があり、最終列に層名（文字列でクラス名、または整数で0-nの連番）が入っていること。
 # created: 2017-07-08
-#----------------------------------------
 import pandas
 import pickle
 import numpy as np
@@ -14,17 +12,21 @@ from keras.models import model_from_json
 # データの読み込み
 data = pandas.read_csv("iris_test.csv")
 #print(data)
-x = (data.iloc[:, 0:4]).values # transform to ndarray
+x = (data.iloc[:, :-1]).values # transform to ndarray
 x = preprocessing.scale(x)     # 次元毎に正規化する
-y = (data.iloc[:, 4:5]).values
-y = [flatten for inner in y for flatten in inner] # transform 2次元 to 1次元 ぽいこと
 
 # 機械学習器を復元
 model = model_from_json(open('model', 'r').read())
 model.load_weights('param.hdf5')
 
+# 出力をラベルに変換する辞書を復元
+label_dict = None
+with open('label_dict.pickle', 'rb') as f:
+	label_dict = pickle.load(f)        # オブジェクト復元
+
 # テスト用のデータを保存
 with open("test_result.csv", "w") as fw:
-	test = model.predict(x)
+	test = model.predict_classes(x)
+	test = [str(label_dict[x]) for x in test] # 文字列に変更（ラベルが数値だった場合に有効）
 	print(test)
-	fw.write(str(test))
+	fw.write("{0}\n".format("\n".join(test)))
