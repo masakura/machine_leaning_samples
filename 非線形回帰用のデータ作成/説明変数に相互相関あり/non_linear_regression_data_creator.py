ff53d74d7eb4b77cddf46495507fa619c2d1919b
Katsuhiro Morishita <morimori.ynct@gmail.com>
2017-07-31 11:52:55 +0900

    非線形回帰問題で使えるサンプルデータを作成する

--- 非線形回帰用のデータ作成/説明変数に相互相関あり/non_linear_regression_data_creator.py
+++ 非線形回帰用のデータ作成/説明変数に相互相関あり/non_linear_regression_data_creator.py
@@ -1,5 +1,4 @@
 # purpose: 非線形回帰分析用のサンプルデータを自動的に作成する
-#  作成途中です！！
 # author: Katsuhiro Morishita
 # created: 2017-07-20
 # license: MIT
@@ -11,9 +10,9 @@ import seaborn as sns
 
 
 w = 5 # 独立変数の数
-wp = 4 # 非独立変数の数
+wp = 0 # 非独立変数の数
 n = 1000 # レコード数
-scale = 500 # 変数の大きさの目安というか
+scale = 10 # 変数の大きさの目安というか
 
 # 独立変数の平均と標準偏差を決める。
 means = [random.uniform(0, scale) for _ in range(w)] # 平均
@@ -39,17 +38,20 @@ for i in range(w):
 for _ in range(wp):
 	c = random.randint(0, len(df.columns)-1) # 引用元のインデックス
 	r = 3 * random.uniform(-1, 1)            # 係数
-	noise = 0.01 * random.uniform(0, scale) * np.random.randn(n) # ノイズ（説明変数毎にノイズの大きさを変える）
+	noise = 0.05 * random.uniform(0, scale) * np.random.randn(n) # ノイズ（説明変数毎にノイズの大きさを変える）
 	df[len(df.columns)] = df[c] * r + noise  # dataframeに追加
 
 # 目的変数を作る
 cof = np.random.randn(len(df.columns)) # 係数を作成する
 open("cof.csv", "w").write(ndarray2str(cof))
-print(cof)
+print("cof", cof)
+exponents = [np.random.randint(1,4) for _ in range(len(df.columns))] # 1 to 3の指数を作成する
+open("exp.csv", "w").write(ndarray2str(exponents))
+print("exp", exponents)
 y = np.array([0]*n)
 for i in range(len(df.columns)):
-	y = y + cof[i] * df[i]
-noise = 0.01 * scale * np.random.randn(n) # ノイズ
+	y = y + cof[i] * (df[i]**exponents[i])
+noise = 0.005 * scale * np.random.randn(n) # ノイズ
 df["noisy y"] = y + noise # noiseなしだと、cofがかなり正確に推定される
 df.columns = ["V{0}".format(x+1) for x in range(len(df.columns))] # 列名を改名する
 
